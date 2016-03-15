681669809cbd2262e9e7d9268b3f5b6e10fdc842
sgigio75 <joseluis.rios@live.com.mx>
2016-03-14 23:14:43 -0700

    Vista de about y detalles.

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 var express = require("express");
 var app = express();
 
-
 app.set("view engine","jade");
-app.get("/login",function(req,res){
 
-res.render("login");
-
-
+app.get("/login",function(req, res) {
+  res.render("login");
 });
 
-app.get("/signup",function(req,res){
-
-res.render("signup");
-
+app.get("/signup",function(req, res) {
+  res.render("signup");
 });
 
-
-app.get("/",function(req,res){
-
-res.render("start");
-
+app.get("/",function(req, res){
+  res.render("start");
 });
-app.get("/proyect",function(req,res){
+
+app.get("/proyect",function(req, res){
     res.render("proyects");
-    
 });
 
 app.get("/productBacklog",function(req,res){
     res.render("prodBacklog");
-    
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
 app.listen(8080);
