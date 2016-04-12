8b7323f522e7d70a894a1542084013122f44ec92
Cristian <CristianFranco@MBP-de-Cristian.lan>
2016-04-11 22:58:02 -0600

    Se añade solo un or en la consulta de los proyectos para que en caso de ser desarrollador en un proyecto pueda ver el mismo

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ app.get("/productBacklog",user.can("anonymousUser"),function(req,res){
 app.get("/simple-cards",user.can("anonymousUser"),function(req,res){
    var data = [];
     Proyecto
-      .find({proyectManager:req.session.user})
+      .find({$or:[{proyectManager:req.session.user},{equipoInvolucdrado:req.session.user}]})
       .populate('proyectManager')
       .populate('equipoInvolucdrado')
       .exec(function (err, proyecto) {
