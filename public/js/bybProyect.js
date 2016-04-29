208d09a643be34f0ec03b66e8a316fda3eed6602
sgigio75 <joseluis.rios@live.com.mx>
2016-04-28 22:13:09 -0600

    Se agrega estílo visual.

--- public/js/bybProyect.js
+++ public/js/bybProyect.js
@@ -1,38 +1,38 @@
 bybApp = angular.module("bybApp",[]);
 bybApp.controller("backlogCtrl",function($scope,$http,$location){
-  
+
     $scope.historias = [];
     $scope.userHistory = {};
-    $scope.socket = io.connect("https://bybproyecttest-carlossn.c9users.io:8080",{'forceNew':true});
-    
+    $scope.socket = io.connect("/",{'forceNew':true});
+
     $scope.getUserHistory = function(id){
         $scope.idProy = id;
         console.log("Se ejecuto la funcion");
         console.log("El Id es"+id);
-        $http.get("https://bybproyecttest-carlossn.c9users.io/api/backlog/" + $scope.idProy).success(function(data){
+        $http.get("/api/backlog/" + $scope.idProy).success(function(data){
             console.log(data);
             $scope.historias = data;
             console.log($scope.userHistorys);
         }).error(function(err){
             console.log(String(err))
-        })  
+        })
     };
-    
+
     $scope.saveUserHistory = function(id){
         console.log("posteando...");
         console.log("El id al hacer el post es: ",id)
         $scope.userHistory.proyectos = id;
         console.log($scope.userHistory);
-        $http.post("https://bybproyecttest-carlossn.c9users.io/api/backlog/"+id,$scope.userHistory).success(function(data){
+        $http.post("/api/backlog/"+id,$scope.userHistory).success(function(data){
             console.log(data);
             $scope.socket.emit("mensajeNuevo",$scope.userHistory);
-            
+
         }).error(function(err){
             console.log(String(err));
         })
         //TODO socket.io
-       
-        
+
+
     }
      $scope.socket.on("enviarMensajes",function(data){
             $scope.historias = data;
