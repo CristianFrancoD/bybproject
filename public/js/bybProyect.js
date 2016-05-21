1e3572cb0d8204ffa7651b6bbd6a6b7bedc7f9a7
Cristian <CristianFranco@MacBook-Pro-de-Cristian.local>
2016-05-21 16:34:40 -0600

    Revert "version nueva"
    This reverts commit 70eddd628ec17cce7db365385a9a7533cf8eb849, reversing
    changes made to d8f11dbc3ac455f281a069afb61ca07555932a02.

--- public/js/bybProyect.js
+++ public/js/bybProyect.js
@@ -3,9 +3,7 @@ bybApp.controller("backlogCtrl",function($scope,$http,$location){
 
     $scope.historias = [];
     $scope.userHistory = {};
-
-    $scope.socket = io.connect("https://",{'forceNew':true},{secure:true});
-
+    $scope.socket = io.connect("http://",{'forceNew':true},{secure:true});
 
     $scope.getUserHistory = function(id){
         $scope.idProy = id;
